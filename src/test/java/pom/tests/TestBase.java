9d1613cf5975f3fa84de76162630960ffd60b00a
andriusjurkus <andrius.p.jurkus@gmail.com>
2024-03-06 17:27:29 +0200

    Added TestBase class

---
+++ src/test/java/pom/tests/TestBase.java
@@ -0,0 +1,17 @@
+package pom.tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import pom.pages.Common;
+import pom.utils.TestListener;
+
+@Listeners(TestListener.class)
+public abstract class TestBase {
+
+    @BeforeMethod
+    public abstract void setUp();
+
+    @AfterMethod
+    public void tearDown() { Common.quitDriver(); }
+}
