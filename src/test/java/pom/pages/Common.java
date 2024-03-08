0852142e1b9cf605a34b0eb785a5e740810a087e
andriusjurkus <andrius.p.jurkus@gmail.com>
2024-03-08 13:39:22 +0200

    Added getElement method to Common class

--- src/test/java/pom/pages/Common.java
+++ src/test/java/pom/pages/Common.java
@@ -1,6 +1,12 @@
 package pom.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pom.utils.Driver;
+
+import java.time.Duration;
 
 public class Common {
     public static void setUpChrome() {
@@ -13,5 +19,22 @@ public class Common {
 
     public static void quitDriver() {
         Driver.quitDriver();
+    }
+
+    private static WebElement getElement(By locator) {
+        return Driver.getDriver().findElement(locator);
+    }
+
+    public static void waitForElementToBeVisible(By locator) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public static void addText(By locator, String text) {
+        getElement(locator).sendKeys(text);
+    }
+
+    public static void clickOnElement(By locator) {
+        getElement(locator).click();
     }
 }
